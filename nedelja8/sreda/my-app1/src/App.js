43926e7f74a470ef56b7a52ba213c6e1a28deeaf
Marko Marković <you@example.com>
2021-01-30 13:42:23 +0100

    domaci

---
+++ nedelja8/sreda/my-app1/src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import {ShoopingList} from './Components/components'
+
+const App = () => {
+  let ime = "Marko"
+
+  //setArr, nije morao biti koristen, cisto sam zbog prakse koristio
+  const [arr, setArrr] = useState([
+    { id:"dgckg-yktgog-kfg",
+      naziv: "Tv samsung 32''",
+      cena: 1000
+    },
+    {
+      id:"fjb-kygl-kygfug",
+      naziv: "IPhone X",
+      cena: 1150
+    },
+    {
+      id:"yjhfuk-jgk-,jfuyl",
+      naziv: "LG phone",
+      cena: 800
+    }
+  ]);
+
+  return (
+    <div>
+      <ShoopingList name={ime} arr={arr}/>
+
+    </div>
+  )
+}
+export default App 
